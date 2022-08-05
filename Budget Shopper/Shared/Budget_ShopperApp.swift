bf4725c1b8a989e3955ace2d265345517ab69c9f
Al <alex.shillingford@icloud.com>
2022-08-05 13:28:59 -0400

    Project commit

---
+++ Budget Shopper/Shared/Budget_ShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_ShopperApp.swift
+//  Shared
+//
+//  Created by William Shillingford on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Budget_ShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
